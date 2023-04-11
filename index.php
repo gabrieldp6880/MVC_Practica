1a82a4eb0cec421b4218d287537acef0b16fcd58
Ivan del Pino <xpy5129@bbvapy.corporativo.bbva.com.py>
2023-04-11 18:58:22 -0300

    Commit desde phpstorm

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
     require_once "./config/APP.php";
     require_once "./controladores/vistasControlador.php";
 
+    //Comentarios
     $plantilla = new vistasControlador();
     $plantilla -> obtener_plantilla_controlador();
